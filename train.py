c284af76c2fac5eb955521d8d88a4c5f7208e723
Viet-Tien Ngoc <tiena2cva@gmail.com>
2021-07-11 22:06:27 +0700

    add train script

--- train.py
+++ train.py
@@ -1,12 +1,30 @@
-from vit.model import ViT
-import tensorflow as tf
-import numpy as np
+from vit.model import ViTBase
+import tensorflow_datasets as tfds
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.optimizers import Adam
 
 if __name__ == "__main__":
-    model = ViT()
-    model.build(input_shape=(8, 224, 224, 3))
+    train_ds = tfds.load("cats_vs_dogs",
+                         split='train[:80%]',
+                         batch_size=64,
+                         shuffle_files=True,
+                         as_supervised=True)
+    val_ds = tfds.load("cats_vs_dogs",
+                       split='train[80%:]',
+                       batch_size=64,
+                       shuffle_files=True,
+                       as_supervised=True)
+
+    model = ViTBase()
+    model.build(input_shape=(64, 224, 224, 3))
     model.summary()
 
-    images = np.random.rand(8, 224, 224, 3)
-    output = model(images)
-    print(output.shape)
+    optimizer = Adam(learning_rate=0.01)
+    loss = SparseCategoricalCrossentropy()
+    model.compile(optimizer, loss=loss,
+                  metrics=['accuracy'])
+
+    model.fit(train_ds,
+              epochs=10,
+              batch_size=64,
+              validation_data=val_ds)

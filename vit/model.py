7ba0b7641d17c299d60d77a0c6b24fa3b0b934f3
bangoc123 <bangoc.gdg@gmail.com>
2021-07-18 17:42:51 +0700

    Add document to encoder + change hidden dim/embed dim to D

--- vit/model.py
+++ vit/model.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import Model
 
 
 class ViT(Model):
-    def __init__(self, num_layers=12, num_heads=12, hidden_dim=768, mlp_dim=3072, num_classes=10, patch_size=16, image_size=224, dropout=0.1, norm_eps=1e-12):
+    def __init__(self, num_layers=12, num_heads=12, D=768, mlp_dim=3072, num_classes=10, patch_size=16, image_size=224, dropout=0.1, norm_eps=1e-12):
         super(ViT, self).__init__()
 
         # Data augmentation
@@ -23,13 +23,13 @@ class ViT(Model):
         ])
 
         # Patch embedding
-        self.embedding = PatchEmbedding(patch_size, image_size, hidden_dim)
+        self.embedding = PatchEmbedding(patch_size, image_size, D)
 
         # Encoder with transformer
         self.encoder = TransformerEncoder(
             num_heads=num_heads,
             num_layers=num_layers,
-            embed_dim=hidden_dim,
+            embed_dim=D,
             mlp_dim=mlp_dim,
             dropout=dropout,
             norm_eps=norm_eps,
@@ -63,7 +63,7 @@ class ViTBase(ViT):
     def __init__(self, num_classes=10, patch_size=16, image_size=224, dropout=0.1, norm_eps=1e-12):
         super().__init__(num_layers=12,
                          num_heads=12,
-                         hidden_dim=768,
+                         D=768,
                          mlp_dim=3072,
                          num_classes=num_classes,
                          patch_size=patch_size,
@@ -76,7 +76,7 @@ class ViTLarge(ViT):
     def __init__(self, num_classes=10, patch_size=16, image_size=224, dropout=0.1, norm_eps=1e-12):
         super().__init__(num_layers=24,
                          num_heads=16,
-                         hidden_dim=1024,
+                         D=1024,
                          mlp_dim=4096,
                          num_classes=num_classes,
                          patch_size=patch_size,
@@ -89,7 +89,7 @@ class ViTHuge(ViT):
     def __init__(self, num_classes=10, patch_size=16, image_size=224, dropout=0.1, norm_eps=1e-12):
         super().__init__(num_layers=32,
                          num_heads=16,
-                         hidden_dim=1280,
+                         D=1280,
                          mlp_dim=5120,
                          num_classes=num_classes,
                          patch_size=patch_size,
